03e466fcd8454d0ab7481a38247871202c2bb232
AdrielRodriguesS <adriels.projetos@gmail.com>
2022-05-23 19:45:45 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -72,15 +72,52 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 		finally {
 			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.closeResultSet(rs);
 		}
 
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List <Seller> list = new ArrayList<>();
+			Map <Integer, Department> map = new HashMap<>(); // mapa para alocar o departamento instanciado
+			
+			while (rs.next()){				
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); // testa se já tem um departamento instanciado
+				
+				if (dep == null) { // se nao tiver departamento instanciado, instancia e escreve no map.
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);				
+				list.add(obj);
+			}
+			return list;			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());			
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
@@ -124,7 +161,7 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 		finally {
 			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.closeResultSet(rs);
 		}
 		
 	}
