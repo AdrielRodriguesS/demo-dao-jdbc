34b3e47327bd7e73f4e5ed9cd103409b4ee81fdf
AdrielRodriguesS <adriels.projetos@gmail.com>
2022-05-23 19:10:48 -0300

    findByDepartment Implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -73,6 +76,58 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 
 	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			
+			rs = st.executeQuery();
+			
+			List <Seller> list = new ArrayList<>();
+			Map <Integer, Department> map = new HashMap<>(); // mapa para alocar o departamento instanciado
+			
+			while (rs.next()){				
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); // testa se já tem um departamento instanciado
+				
+				if (dep == null) { // se nao tiver departamento instanciado, instancia e escreve no map.
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);				
+				list.add(obj);
+			}
+			return list;			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());			
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+	}
 	
 	// Após execução da Query SQL, os dados são alocados num ResultSet.
 	// O método abaixo instancia um objeto e resgata os dados do ResultSet gerado pela consulta.
@@ -96,12 +151,6 @@ public class SellerDaoJDBC implements SellerDao {
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
 		obj.setDeparment(dep);
 		return obj;
-	}
-
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
